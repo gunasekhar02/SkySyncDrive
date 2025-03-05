e6de3576dd606d444b1f1eecd5938ec4715f98ef
Guna Sekhar Reddy Peddireddygari <101317857+gunasekhar02@users.noreply.github.com>
2025-03-05 23:34:18 +0530

    SKY-07 made storage chart dynamic according to api response

--- sky-sync-drive/src/apiservice.service.ts
+++ sky-sync-drive/src/apiservice.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { UserLogin,UserSignin } from './app/Models/user.models';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { UserLogin, UserSignin } from './app/Models/user.models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiserviceService {
+  private apiAuthUrl = 'https://localhost:7103/api/Auth';
+  private fileManagerUrl = 'https://localhost:7177/api/files';
+  constructor(private httpClient: HttpClient) {}
 
-  private apiAuthUrl = 'https://localhost:7103/api/Auth'; 
-  constructor(private httpClient:HttpClient) {
+  // Method to get the token (e.g., from localStorage)
+  getToken(): string {
+    return localStorage.getItem('token') || '';
   }
 
-  userSignIn(userSignIn:UserSignin){
+  // Method to create headers with the token
+  private getHeaders(): HttpHeaders {
+    const token = this.getToken();
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
+  userSignIn(userSignIn: UserSignin) {
     return this.httpClient.post(`${this.apiAuthUrl}/signup`, userSignIn);
   }
 
-  userLogin(userLogin:UserLogin){
+  userLogin(userLogin: UserLogin) {
     return this.httpClient.post(`${this.apiAuthUrl}/login`, userLogin);
   }
+
+  getAllMediaOfUser() {
+    const headers = this.getHeaders(); // Get headers with the token
+    return this.httpClient.get(`${this.fileManagerUrl}`, { headers })  }
 }
